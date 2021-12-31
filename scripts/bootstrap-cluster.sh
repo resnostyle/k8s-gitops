b22e7d73c02d3a3a44257fcce3e46e81f080240c
Bryan Pearson <bwp.pearson@gmail.com>
2021-12-31 11:07:50 -0500

    Sops (#75)
    poor branching here.
    merging many additions and changes into repo.
    
    started with sops, but didnt finsh it. 
    added new services.

--- scripts/bootstrap-cluster.sh
+++ scripts/bootstrap-cluster.sh
@@ -15,21 +15,6 @@ message() {
       echo "######################################################################"
 }
 
-installArgocd() {
-  kubectl create ns argocd
-#  kubectl apply -k ../argo-cd/
-#  kubectl delete -k ../argo-cd/
-#   helm install argo-cd argoproj/ --namespace argocd
-}
-
-configureArgocd(){
-  kubens argocd
-#  kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-  argocd repo add git@gitlab.com:resnostyle/helm-charts.git --ssh-private-key-path ~/.ssh/id_ed25519
-  argocd repo add git@github.com:resnostyle/k8s-gitops.git --ssh-private-key-path ~/.ssh/id_ed25519 --insecure-skip-server-verification
-  kubens -
-}
-
 deployArgoSets(){
   kubens argocd
   argocd app create kube-system --repo git@github.com:resnostyle/k8s-gitops.git --path argoproj/system --dest-server https://kubernetes.default.svc --self-heal --sync-policy auto
